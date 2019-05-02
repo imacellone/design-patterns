8d4cfd8c97019ea92805dd6f6dc8d0f485d276c0
Italo Macellone <italomacellone@gmail.com>
2019-05-02 21:32:55 +0200

    Better output strategy design pattern exercise

--- src/main/java/com/macellone/designpatterns/strategy/exercise/Main.java
+++ src/main/java/com/macellone/designpatterns/strategy/exercise/Main.java
@@ -6,12 +6,12 @@ public class Main {
 		RealizadorDeInvestimento realizador = new RealizadorDeInvestimento();
 		
 		ResultadoInvestimento moderado = realizador.investir(conta, new Moderado());
+		System.out.println("Moderado: Investido: " + moderado.getValorInvestido() + " Bruto: " + moderado.getValorBrutoRetornado() + " Liquido: " + moderado.getValorLiquidoRetornado() + " Saldo: " + conta.getSaldo());
+		
 		ResultadoInvestimento conservador = realizador.investir(conta, new Conservador());
+		System.out.println("Conservador: Investido: " + conservador.getValorInvestido() + " Bruto: " + conservador.getValorBrutoRetornado() + " Liquido: " + conservador.getValorLiquidoRetornado() + " Saldo: " + conta.getSaldo());
+		
 		ResultadoInvestimento arrojado = realizador.investir(conta, new Arrojado());
-		
-		
-		System.out.println("Moderado: Investido: " + moderado.getValorInvestido() + " Bruto: " + moderado.getValorBrutoRetornado() + " Liquido: " + moderado.getValorLiquidoRetornado());
-		System.out.println("Conservador: Investido: " + conservador.getValorInvestido() + " Bruto: " + conservador.getValorBrutoRetornado() + " Liquido: " + conservador.getValorLiquidoRetornado());
-		System.out.println("Arrojado: Investido: " + arrojado.getValorInvestido() + " Bruto: " + arrojado.getValorBrutoRetornado() + " Liquido: " + arrojado.getValorLiquidoRetornado());
+		System.out.println("Arrojado: Investido: " + arrojado.getValorInvestido() + " Bruto: " + arrojado.getValorBrutoRetornado() + " Liquido: " + arrojado.getValorLiquidoRetornado() + " Saldo: " + conta.getSaldo());
 	}
 }
